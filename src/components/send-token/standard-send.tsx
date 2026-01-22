a87f04479f6612beed9f4346c729b69d55a036f8
bourdillion <isaacakhigbe22@gmail.com>
2026-01-22 21:59:50 +0100

    fix: background color and border

--- src/components/send-token/standard-send.tsx
+++ src/components/send-token/standard-send.tsx
@@ -175,7 +175,7 @@ export default function StandardAirdrop({ onGasUsed }: StandardAirdropProps) {
 
     return (
         <div className="max-w-3xl mx-auto">
-            <div className="bg-slate-900/50 backdrop-blur-xl border border-orange-500/30 rounded-2xl p-8 shadow-2xl">
+            <div className=" backdrop-blur-xl  rounded-2xl p-8 shadow-2xl">
 
                 <div className="space-y-6">
                     <InputForm 
