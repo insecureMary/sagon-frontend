f2d6edc433e770f9ae72b4b842fa75e834b0c2a0
bourdillion <isaacakhigbe22@gmail.com>
2026-01-25 15:40:56 +0100

    fix: correct providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,61 +1,15 @@
 "use client"
 
-<<<<<<< HEAD
-import config from "@/rainbowKitConfig"
-import { type ReactNode } from "react"
-import { WagmiProvider } from "wagmi"
-import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit"
-=======
 import config from "@/rainbowkitConfig"
 import { type ReactNode } from "react"
 import { WagmiProvider } from "wagmi"
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit" 
->>>>>>> 99d4f54a827971fc7cfad9399cfef28905be4212
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
 import { useTheme } from "next-themes"
 import "@rainbow-me/rainbowkit/styles.css"
 
 function RainbowKitWrapper({ children }: { children: ReactNode }) {
-<<<<<<< HEAD
-    const { resolvedTheme } = useTheme()
-
-    return (
-        <RainbowKitProvider
-            coolMode
-            modalSize="compact"
-            theme={resolvedTheme === 'dark' ? darkTheme({
-                accentColor: '#3B33B1',
-                accentColorForeground: 'white',
-                borderRadius: 'large',
-                fontStack: 'system',
-                overlayBlur: 'small',
-            }) : lightTheme({
-                accentColor: '#3B33B1',
-                accentColorForeground: 'white',
-                borderRadius: 'large',
-                fontStack: 'system',
-                overlayBlur: 'small',
-            })}
-        >
-            {children}
-        </RainbowKitProvider>
-    )
-}
-
-export function Providers(props: { children: ReactNode }) {
-    const [queryClient] = useState(() => new QueryClient())
-
-    return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <RainbowKitWrapper>
-                    {props.children}
-                </RainbowKitWrapper>
-            </QueryClientProvider>
-        </WagmiProvider>
-    )
-=======
   const { resolvedTheme } = useTheme()
   
   return (
@@ -93,5 +47,4 @@ export function Providers(props: { children: ReactNode }) {
       </QueryClientProvider>
     </WagmiProvider>
   )
->>>>>>> 99d4f54a827971fc7cfad9399cfef28905be4212
 }
